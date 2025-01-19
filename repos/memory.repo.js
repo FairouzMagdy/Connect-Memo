b461653ab89cfc61e5a3bf9c2f691062eb990c37
fairouzmagdy <fairouzmagdy219@gmail.com>
2025-01-19 17:46:36 +0200

    fix: handle case when connection is blocked

--- repos/memory.repo.js
+++ repos/memory.repo.js
@@ -1,4 +1,5 @@
 const Memory = require("../models/memory.model");
+const Connection = require("../models/connection.model");
 const { mediaService } = require("../services/media.service");
 const { imageKitPayloadBuilder } = require("../utils/media.util");
 
@@ -88,6 +89,18 @@ class MemoryRepository {
       }).populate("createdBy privacy.sharedWith viewedBy");
 
       if (!memory) throw new Error("No memory found with this id");
+
+      const connection = await Connection.findOne({
+        $or: [
+          { from: memory.createdBy, to: userId },
+          { from: userId, to: memory.createdBy },
+        ],
+        status: "blocked",
+      });
+
+      if (connection) {
+        throw new Error("You are blocked and cannot view this memory");
+      }
 
       if (
         !memory.createdBy.equals(userId) &&
@@ -210,6 +223,18 @@ class MemoryRepository {
 
   async shareMemoryWithUser(memoryId, currentUserId, userId) {
     try {
+      const connection = await Connection.findOne({
+        $or: [
+          { from: currentUserId, to: userId },
+          { from: userId, to: currentUserId },
+        ],
+        status: "blocked",
+      });
+
+      if (connection) {
+        throw new Error("Cannot share memory with a blocked user");
+      }
+
       const memory = await Memory.findOneAndUpdate(
         { _id: memoryId, createdBy: currentUserId },
         { $addToSet: { "privacy.sharedWith": userId } },
@@ -226,10 +251,30 @@ class MemoryRepository {
   async getAllMemories(userId) {
     // public and shared-with memories
     try {
+      const blockedConnections = await Connection.find({
+        $or: [
+          { from: userId, status: "blocked" },
+          { to: userId, status: "blocked" },
+        ],
+      });
+
+      const blockedUsers = new Set(
+        blockedConnections.map((connection) =>
+          connection.from === userId ? connection.to : connection.from
+        )
+      );
+
+      console.log(blockedUsers);
+
       const memories = await Memory.find({
         $or: [{ "privacy.zone": "public" }, { "privacy.sharedWith": userId }],
       });
-      return memories;
+
+      const validMemories = memories.filter(
+        (memory) => !blockedUsers.has(memory.createdBy.id.toString())
+      );
+
+      return validMemories;
     } catch (error) {
       throw error;
     }
